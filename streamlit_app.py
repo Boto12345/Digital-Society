371cc4c62110cbc4c6e8fed3abc7048ec2a6ba46
Boto12345 <157336648+Boto12345@users.noreply.github.com>
2024-02-17 02:13:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
-#from llama_index import VectorStoreIndex, ServiceContext, Document
-#from llama_index.llms import OpenAI
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+pip install google-generativeai
 from google.generativeai import GenerativeAiClient
-#import openai
+import openai
 from llama_index import SimpleDirectoryReader
 
 st.set_page_config(page_title="Digital Society Teachers Assistant", page_icon="", layout="centered", initial_sidebar_state="auto", menu_items=None)
@@ -15,28 +16,24 @@ if "messages" not in st.session_state.keys(): # Initialize the chat messages his
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question about any Digital Society related query"}
     ]
-
 @st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Loading hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=Gemini(model="bard-large", temperature=0.1, system_prompt="You are the IB Digital Society teacher's assistant. You should answer the students' questions based on the data that you have. If a student requests you to revise a specific topic, you should be able to display the important contents of the chapter in bulletin points. If a question is not relevant to Digital Society, request the student to ask a relevant question. If you do not know the answer, request the student to ask their Digital Society teacher. Keep your answers technical and based on facts – do not hallucinate features.", client=client))
+        #service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4", temperature=0.1, system_prompt="You are the IB Digital Society teacher's assistant. You should answer the students' questions based on the data that you have. If a student requests you to revise a specific topic, you should be able to display the important contents of the chapter in bulletin points. If a question is not relevant to Digital Society, request the student to ask a relevant question. If you do not know the answer, request the student to ask their Digital Society teacher. Keep your answers technical and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=Gemini(model="bard-large", temperature=0.1, system_prompt="You are the IB Digital Society teacher's assistant. You should answer the students' questions based on the data that you have. If a student requests you to revise a specific topic, you should be able to display the important contents of the chapter in bulletin points. If a question is not relevant to Digital Society, request the student to ask a relevant question. If you do not know the answer, request the student to ask their Digital Society teacher. Keep your answers technical and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
 index = load_data()
-
 if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
         st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
-
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
-
 for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
-
 # If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
